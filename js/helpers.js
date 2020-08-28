f614987aef183e04b5f2440efc720082f6551c34
ELODIE WEBDESIGN <elodie@MacBook-Pro-de-Elodie.local>
2020-08-28 16:51:30 +0200

    update jsdoc helpers.js.html

--- js/helpers.js
+++ js/helpers.js
@@ -1,22 +1,46 @@
-/* NodeList globale */
+/**
+ * NodeList globale
+ */
+/** @global */
 (function (window) {
 	'use strict';
 
-	// Récupérez les éléments par le sélecteur CSS:
+	/**
+	 * Récupère un élément avec querySelector (qs) et le sélecteur CSS
+	 * @param {string} selector Le sélecteur css de l'élément
+	 *  @param {string} scope Le scope de l'élément (si pas renseigné, document par défaut)
+	 */
 	window.qs = function (selector, scope) {
 		return (scope || document).querySelector(selector);
 	};
+
+	/**
+	 * Récupère plusieurs éléments avec querySelectorAll (qsa) et le sélecteur CSS
+	 * @param {string} selector Le sélecteur css de l'élément
+	 *  @param {string} scope Le scope de l'élément (si pas renseigné, document par défaut)
+	 */
 	window.qsa = function (selector, scope) {
 		return (scope || document).querySelectorAll(selector);
 	};
 
-	// ajouter le wrapper Event Listener:
+	/**
+	 * Ajoute un écouteur d'évènement à l'élément ciblé
+	 * @param {element} target L'élément ciblé
+	 *  @param {string} type Le type de l'évènement (click, change...)
+	 *  @param {string} callback La réponse en cas d'évènement
+	 *  @param {Boolean} useCapture indique si l'évènement est envoyé au listener enregistré avant d'être distribué à tout EventTarget (https://developer.mozilla.org/fr/docs/Web/API/EventTarget/addEventListener)
+	 */
 	window.$on = function (target, type, callback, useCapture) {
 		target.addEventListener(type, callback, !!useCapture);
 	};
 
-	// Attachez un gestionnaire à l'événement pour tous les éléments qui correspondent au sélecteur,
-	// maintenant ou dans le futur, basé sur un élément racine
+	/**
+	 * Ajoute un écouteur d'évènement à tous les éléments qui correspondent au sélecteur passé dans la fonction.
+	 * @param {element} target L'élément ciblé
+	 *  @param {string} selector Le sélecteur css de l'élément ciblé
+	 *  @param {string} seltypeector Le type de l'event
+	 *  @param {string} handler Callback exécuté
+	 */
 	window.$delegate = function (target, selector, type, handler) {
 		function dispatchEvent(event) {
 			let targetElement = event.target;
@@ -27,15 +51,17 @@
 				handler.call(targetElement, event);
 			}
 		}
-
 		// https://developer.mozilla.org/en-US/docs/Web/Events/blur
 		let useCapture = type === 'blur' || type === 'focus';
 
 		window.$on(target, type, dispatchEvent, useCapture);
 	};
 
-	// Trouvez le parent de l'élément avec le nom de balise donné:
-	// $parent(qs('a'), 'div');
+	/**
+	 * Trouve l'élément parent qui porte le tag suivant : $parent(qs('a'), 'div');
+	 * @param {element} element L'élément ciblé
+	 *  @param {string} tagName Le sélecteur css de l'élément ciblé
+	 */
 	window.$parent = function (element, tagName) {
 		if (!element.parentNode) {
 			return;
@@ -45,7 +71,6 @@
 		}
 		return window.$parent(element.parentNode, tagName);
 	};
-
 	// Autoriser la boucle sur les nœuds en chaînant:
 	// qsa('.foo').forEach(function () {})
 	NodeList.prototype.forEach = Array.prototype.forEach;
