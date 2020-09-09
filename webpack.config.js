112f9eb038c2f2513dc6f1d5de494d510b2681a6
ELODIE WEBDESIGN <elodie@macbook-pro-de-elodie.home>
2020-09-09 08:54:53 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,17 @@
 // Webpack uses this to work with directories
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
-
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: ["./src/javascript/app.js", "./assets/style/main.css", ],
+    entry: "./src/javascript/app.js",
 
-    // Path and filename of your result bundle.
-    // Webpack will bundle all JavaScript into this file
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: './assets/style/',
-                    },
-                },
-                'css-loader',
-            ],
-        }, ]
-    },
-    plugins: [new MiniCssExtractPlugin()],
-
-    mode: 'development'
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 };
